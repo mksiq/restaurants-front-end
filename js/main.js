7b6a032885cfb01b4b1ff02a443cc67eed175d95
root <root@LAPTOP-LKOK4UDA.localdomain>
2021-01-28 19:41:01 -0500

    modal now loads map by leaflet

--- js/main.js
+++ js/main.js
@@ -10,10 +10,9 @@ const uri =
   "https://web422-assignment1-msiqueira.herokuapp.com/api/restaurants";
 
 const tableTemplate = _.template(`
-    
     <% _.forEach(restaurantData, function(restaurant) {
         %>  
-        <tr scope="row" data-id="<%- restaurant._id %>" data-bs-toggle="modal" data-bs-target="#leaftlet">
+        <tr scope="row" data-id="<%- restaurant._id %>" data-bs-toggle="modal" data-bs-target="#restaurant-modal">
         <td><%- restaurant.name %></td>
         <td><%- restaurant.cuisine %></td>
         <td><%- restaurant.address.building%> <%- restaurant.address.street  %></td>
@@ -44,69 +43,65 @@ $(function () {
       res.json().then((data) => {
         currentRestaurant = data.restaurant;
         $(".modal-title").html(currentRestaurant.name);
-        $("#restaurant-address").html(`${currentRestaurant.address.building} ${currentRestaurant.address.street}`)
+        $("#restaurant-address").html(
+          `${currentRestaurant.address.building} ${currentRestaurant.address.street}`
+        );
+        loadMap();
       })
     );
   });
 
   nextPage();
+
   previousPage();
+  $('#restaurant-modal').on('hidden.bs.modal', function() {
+    if(map) {
+        map.remove();
+    }
+  });
 });
 
 // move to next page
 function nextPage() {
-    $("#next").on("click", function (e) {
-        e.preventDefault();
-        page++;
-        $("#restaurant-table tbody").empty();
-        loadRestaurantData();
-        $("#page").html(page);
-        $("#previous").removeClass("disabled");
-    });
+  $("#next").on("click", function (e) {
+    e.preventDefault();
+    page++;
+    $("#restaurant-table tbody").empty();
+    loadRestaurantData();
+    $("#page").html(page);
+    $("#previous").removeClass("disabled");
+  });
 }
 
 // move to previous page
 function previousPage() {
-    $("#previous").on("click", function (e) {
-        e.preventDefault();
-        if($("#page").html() > 1) {
-            page--;
-            $("#restaurant-table tbody").empty();
-            loadRestaurantData();
-            $("#page").html(page);
-        }
-        if(page == 1) {
-            $("#previous").addClass("disabled");
-        }
-    });
+  $("#previous").on("click", function (e) {
+    e.preventDefault();
+    if ($("#page").html() > 1) {
+      page--;
+      $("#restaurant-table tbody").empty();
+      loadRestaurantData();
+      $("#page").html(page);
+    }
+    if (page == 1) {
+      $("#previous").addClass("disabled");
+    }
+  });
 }
-// $(document).ready(function () {
-//   // const test = new Promise(fillTable(restaurantData)).then( ready => console.log(ready));
 
-//   $("#next-page").click(function (e) {
-//     e.preventDefault();
-//     page++;
-
-//     $("#restaurant-table tbody").remove();
-
-//     fetch(
-//       `http://localhost:8080/api/restaurants?page=${page}&perPage=${perPage}&borough=Bronx`
-//     ).then((res) => {
-//       res.json().then((data) => {
-//         restaurantData = data;
-//         let tableTemplate = _.template(` <tbody>
-//         <% _.forEach(restaurantData, function(restaurant) {
-//             %>
-//             <tr id="<%- restaurant.restaurant-id %>">
-//             <td><%- restaurant.name %></td>
-//             <td><%- restaurant.cuisine %></td>
-//             <td><%- restaurant.address.building%> <%- restaurant.address.street  %></td>
-//             <td><%- avg(restaurant.grades).toFixed(2) %></td>
-//             </tr>  <% }); %>
-//             </tbody>`);
-//         let rows = tableTemplate({ restaurantData: restaurantData });
-//         $("#restaurant-table").append(tableTemplate);
-//       });
-//     });
-//   });
-// });
+function loadMap() {
+  map = new L.Map("leaflet", {
+    center: [
+      currentRestaurant.address.coord[1],
+      currentRestaurant.address.coord[0],
+    ],
+    zoom: 18,
+    layers: [
+      new L.TileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
+    ],
+  });
+  L.marker([
+    currentRestaurant.address.coord[1],
+    currentRestaurant.address.coord[0],
+  ]).addTo(map);
+}
